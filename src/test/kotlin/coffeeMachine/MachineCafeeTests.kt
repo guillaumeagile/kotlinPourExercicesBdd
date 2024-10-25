9c69c54120b71fab0f2dad76a16c761beec31a46
guillaume prof <guillaume.saintetienne@iter.org>
2024-10-25 21:27:22 +0200

    lisibilité pour les étudiants

--- src/test/kotlin/coffeeMachine/MachineCafeeTests.kt
+++ src/test/kotlin/coffeeMachine/MachineCafeeTests.kt
@@ -8,16 +8,19 @@ import org.example.coffeeMachine.MachineCafee
 class MachineCafeeTests : BehaviorSpec({
 
     given("un machine à café") {
+
         val machine = MachineCafee()
 
         `when`("je demande un thé avec un sucre et une touillette") {
+
             var command = machine.Command( drink = Drink.Tea, sugar = 1, stick = 1 )
+
             then("le message est envoyé au Drink Maker") {
+
                 command shouldBe "T:1:0"
+
             }
-
         }
-
     }
 }
 )
