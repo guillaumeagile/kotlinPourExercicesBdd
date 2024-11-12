941dd684839356b2a1a360cbcb5db2f34d170d4f
guillaume prof <guillaume.saintetienne@iter.org>
2024-11-12 19:15:21 +0100

    example mapping wedding planner

---
+++ src/test/kotlin/weddingPlanner/TenueDeMaries/TenuesSpecificationsExecutables.kt
@@ -0,0 +1,93 @@
+package weddingPlanner.TenueDeMaries
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class TenuesSpecificationsExecutables : BehaviorSpec({
+
+    //user story
+    //en tant que futur epoux-se, je souhaite obtenir une tenue afin de participer à la"
+    //            "cérémonie de mariage
+
+    //regle
+    // le prix de la robe plus costume doit etre inférieur à 10% du budget local
+
+    context("le prix de la robe plus costume doit etre inférieur à 10% du budget local") {
+
+        given("le couple défini un budget total de 15000€") {
+
+            val budgetTotal = 15000
+
+            and("le prix de la robe est de 1000€ ") {
+
+                val prixDeLaRobe = 1000
+
+                `when` ("je veux savoir quel est le prix maximum pour le costume ") {
+
+                    val prixMaximumCostume = CalculPrixCostume(prixDeLaRobe , budgetTotal)
+
+                    then("le résultat est 500€" ){
+                        prixMaximumCostume shouldBe 500
+                    }
+                }
+            }
+
+            and("le prix de la robe est de 1250€ ") {
+
+                val prixDeLaRobe = 1250
+
+                `when` ("je veux savoir quel est le prix maximum pour le costume ") {
+
+                    val prixMaximumCostume = CalculPrixCostume(prixDeLaRobe , budgetTotal)
+
+                    then("le résultat est 250" ){
+                        prixMaximumCostume shouldBe 250
+                    }
+                }
+            }
+        }
+
+        given("le couple défini un budget total de 20000€") {
+
+            val budgetTotal = 20000
+
+            and("le prix de la robe est de 1500€") {
+
+                val prixDeLaRobe = 1500
+
+                `when` ("je veux savoir quel est le prix maximum pour le costume ") {
+
+                    val prixMaximumCostume = CalculPrixCostume(prixDeLaRobe , budgetTotal)
+
+                    then("le résultat est 500€" ){
+                        prixMaximumCostume shouldBe 500
+                    }
+                }
+            }
+
+
+            and("le prix de la robe est de 2100€") {
+
+                val prixDeLaRobe = 2100
+
+                `when` ("je veux savoir quel est le prix maximum pour le costume ") {
+
+                    val prixMaximumCostume = CalculPrixCostume(prixDeLaRobe , budgetTotal)
+
+                    then("le résultat est 0€" ){
+                        prixMaximumCostume shouldBe 0
+                    }
+                }
+            }
+
+
+        }
+    }})
+
+
+
+
+
+fun CalculPrixCostume(prixDeLaRobe: Int, budgetTotal: Int): Int {
+    return  Math.max(  (budgetTotal * 0.1).toInt() - prixDeLaRobe, 0)
+}
